298eb55e8f58f25508fb85c413b792fcf6c04431
Ilya Hancharyk <Ilya_Hancharyk@epam.com>
2021-06-11 16:39:25 +0300

    Add integrations page extensions

--- ui/src/components/examplePluginTab/examplePluginTab.jsx
+++ ui/src/components/examplePluginTab/examplePluginTab.jsx
@@ -1,15 +1,10 @@
-import PlusMarkIcon from 'icons/plus-mark.svg';
-
 export const ExamplePluginTab = (props) => {
   const {
-    lib: { React, Parser },
+    lib: { React },
   } = props;
 
   return (
     <div>
-      <div style={{ width: '13px', height: '13px', marginRight: '13px' }}>
-        {Parser(PlusMarkIcon)}
-      </div>
       Hello world!
     </div>
   );
